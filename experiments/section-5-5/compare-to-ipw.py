a578ca799e10c239a58e9ea7def063e1e9a705aa
nikolajthams <ctj458@a00280.science.domain>
2022-08-11 15:08:12 +0200

    Add double robustness to IPW comparison

--- experiments/section-5-5/compare-to-ipw.py
+++ experiments/section-5-5/compare-to-ipw.py
@@ -15,10 +15,11 @@ except NameError: print("Cannot load testing module")
 from resample_and_test import ShiftTester
 
 # Help functions
-def e(n=1, mu=0): return np.random.normal(size=(n, 1), loc=mu)  # Gaussian noise
-def cb(*args): return np.concatenate(args, axis=1)  # Col bind
+def e(n=1, mu=0): 
+    return np.random.normal(size=(n, 1), loc=mu)  # Gaussian noise
+def cb(*args): 
+    return np.concatenate(args, axis=1)  # Col bind
 p = norm.pdf
-np.random.seed(1)
 
 sigma_eps = 2.0
 # Simulate data from a Gaussian SCM
@@ -29,53 +30,91 @@ def scm(n):
     return cb(X1, X2, X3)
 
 # Weight function
-def get_weight(target_mean=1):
-    def weight(X): return(p(X[:,1], loc=target_mean, scale=sigma_eps/2)/p(X[:, 1], loc=X[:,0], scale=sigma_eps))
+def get_weight(target_mean=1, fit=False):
+    def weight(X): 
+        if not fit:
+            return(p(X[:,1], loc=target_mean, scale=sigma_eps/2)/p(X[:, 1], loc=X[:,0], scale=sigma_eps))
+        else:
+            model = sm.OLS(X[:,2], sm.add_constant(X[:,0])).fit()
+            old_mean = model.fittedvalues
+            old_scale = np.sqrt(model.scale)
+            return(p(X[:,1], loc=target_mean, scale=old_scale/2)/p(X[:, 1], loc=old_mean, scale=old_scale))
     return weight
 
+
+def get_outcome(fit=False):
+    def mu(X):
+        if not fit:
+            return X[:,1] - X[:,0]
+        else:
+            return sm.OLS(X[:,2], sm.add_constant(X[:,[0,1]])).fit().fittedvalues
+    return mu
+
 # Test function
-def T(X): return 1*(sm.stats.ztest(X[:, 2], value=0)[1] < 0.05)
-
+def get_T(fit=False):
+    def T(X): 
+        return 1*(sm.stats.ztest(X[:, 2], value=0)[1] < 0.05)
+    return T
 # IPW test function
-def T_ipw(X, target_mean):
-    n_ = X.shape[0]
-    z = get_weight(target_mean)(X)*X[:,2]
-    ipw_estimate = z.mean()
-    sd = np.sqrt(z.var())
-    return 1-(np.abs(ipw_estimate) < 1.96*sd/np.sqrt(n_))
-
+def get_T_ipw(target_mean, fit=False):
+    def T_ipw(X):
+        n_ = X.shape[0]
+        z = get_weight(target_mean, fit=fit)(X)*X[:,2]
+        ipw_estimate = z.mean()
+        sd = np.sqrt(z.var())
+        return 1-(np.abs(ipw_estimate) < 1.96*sd/np.sqrt(n_))
+    return T_ipw
 # Clipped IPW test function
-def T_ipw_clipped(X, target_mean):
-    n_ = X.shape[0]
-    z = get_weight(target_mean)(X)
+def get_T_ipw_clipped(target_mean, fit=False):
+    def T_ipw_clipped(X):
+        n_ = X.shape[0]
+        z = get_weight(target_mean,fit=fit)(X)
 
-    # Clip weights
-    z = np.minimum(z, z[np.argsort(z)[-10:]].min())
-    z = z*X[:,2]
-    ipw_estimate = z.mean()
-    sd = np.sqrt(z.var())
+        # Clip weights
+        z = np.minimum(z, z[np.argsort(z)[-10:]].min())
+        z = z*X[:,2]
+        ipw_estimate = z.mean()
+        sd = np.sqrt(z.var())
 
-    return 1-(np.abs(ipw_estimate) < 1.96*sd/np.sqrt(n_))
+        return 1-(np.abs(ipw_estimate) < 1.96*sd/np.sqrt(n_))
+    return T_ipw_clipped
+
+# Doubly robust test function
+def get_T_doubly_robust(target_mean, fit=False):
+    def T_doubly_robust(X):
+        n_ = X.shape[0]
+        z = get_weight(target_mean, fit)(X)
+        
+        # Outcome model
+        mu = get_outcome(fit)
+        dr_summands = (mu(X) + z*(X[:,2] - mu(X)))
+        dr_estimate = dr_summands.mean()
+        dr_sd = np.sqrt(dr_summands.var())
+
+        return 1-(np.abs(dr_estimate) < 1.96*dr_sd/np.sqrt(n_))
+    return T_doubly_robust
 
 # Cutoff for tuning of m
 cutoff = np.quantile(np.random.uniform(size=(1000, 15)).min(axis=1), 0.05)
 
 # Loop parameters
 target_mean_range = np.linspace(1, 6, num=11)
-test_range = {"Resampling": T, "IPW": T_ipw, "IPWClipped": T_ipw_clipped}
+test_range = {"Resampling": get_T, "IPW": get_T_ipw, "IPWClipped": get_T_ipw_clipped, "DR": get_T_doubly_robust}
+fit_range = [True, False]
 n = 100
 
-combinations = list(product(target_mean_range, test_range.keys()))
+combinations = list(product(target_mean_range, test_range.keys(), fit_range))
 
 
 def conduct_experiment(i=None):
+    np.random.seed(i)
     out = []
-    for target_mean, test in combinations:
+    for target_mean, test, fit in combinations:
         X = scm(n)
 
         if test =="Resampling":
             try:
-                psi = ShiftTester(get_weight(target_mean), test_range[test], rate=None, replacement="NO-REPL-reject", reject_retries=100)
+                psi = ShiftTester(get_weight(target_mean, fit=fit), test_range[test](fit), rate=None, replacement="NO-REPL-reject", reject_retries=100)
                 m = psi.tune_m(X, j_x=[0], j_y=[1], gaussian=True,
                                cond=[0], const=target_mean,
                                m_factor=1.3, p_cutoff=cutoff, replacement=False,
@@ -87,7 +126,7 @@ def conduct_experiment(i=None):
                 out.append(np.nan)
         else:
             try:
-                out.append(test_range[test](X, target_mean))
+                out.append(test_range[test](target_mean=target_mean, fit=fit)(X))
             except:
                 out.append(np.nan)
     return out
@@ -109,8 +148,9 @@ if __name__ == '__main__':
     # Pack as data frame
     df = pd.DataFrame(
         [(x/c, *v, *proportion_confint(x, c, method="binom_test"), c) for x, v, c in zip(res, combinations, counts)],
-        columns=["RejectRate", "TargetMean", "Test", "Lower", "Upper", "Count"])
+        columns=["RejectRate", "TargetMean", "Test", "Fit", "Lower", "Upper", "Count"])
 
     # Export to R for ggplotting
     df['RejectRate'] = df["RejectRate"].replace(np.NaN, "NA")
+    print(df)
     df.to_csv("experiments/section-5-5/compare-to-ipw.csv")
